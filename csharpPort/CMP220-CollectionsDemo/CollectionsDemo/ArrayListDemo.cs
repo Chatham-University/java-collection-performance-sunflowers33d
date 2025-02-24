a8285c1ded8f73f2fd6be449de37e2cf692700ab
sunflowers33d <37161028+sunflowers33d@users.noreply.github.com>
2025-02-23 20:56:15 -0500

    continue attempts to run LinkedListDemo (not working)
    c# doesn't appear to have a simple .add method as does the java version of LinkedLists

--- csharpPort/CMP220-CollectionsDemo/CollectionsDemo/ArrayListDemo.cs
+++ csharpPort/CMP220-CollectionsDemo/CollectionsDemo/ArrayListDemo.cs
@@ -1,15 +1,18 @@
-﻿// public class ArrayListDemo {
-//     private final ArrayList<Integer> nums;
-//
-//     public ArrayListDemo(int howManyNums, Random rand) {
-//         nums = new ArrayList<Integer>();
-//         for(int i = 0; i < howManyNums; i++) {
-//             nums.add(rand.nextInt(howManyNums));
-//         }
-//
-//         System.out.println("The first few numbers are: ");
-//         for(int i = 0; i < 6; i++){
-//             System.out.println(nums.get(i));
-//         }
-//     }
-// }
+﻿using System;
+using System.Collections.Generic;
+
+public class ArrayListDemo {
+    private ArrayList<int> nums;
+
+    public ArrayListDemo(int howManyNums, Random rand) {
+        nums = new ArrayList<Integer>();
+        for(int i = 0; i < howManyNums; i++) {
+            nums.add(rand.nextInt(howManyNums));
+        }
+
+        System.out.println("The first few numbers are: ");
+        for(int i = 0; i < 6; i++){
+            Console.WriteLine(nums.get(i));
+        }
+    }
+}
--- csharpPort/CMP220-CollectionsDemo/CollectionsDemo/LinkedListDemo.cs
+++ csharpPort/CMP220-CollectionsDemo/CollectionsDemo/LinkedListDemo.cs
@@ -1,15 +1,21 @@
-﻿// public class LinkedListDemo {
-//     private final LinkedList<Integer> nums;
-//
-//     public LinkedListDemo(int howManyNums, Random rand) {
-//         nums = new LinkedList<Integer>();
-//         for(int i = 0; i < howManyNums; i++) {
-//             nums.add(rand.nextInt(howManyNums));
-//         }
-//
-//         System.out.println("The first few numbers are: ");
-//         for(int i = 0; i < 6; i++){
-//             System.out.println(nums.get(i));
-//         }
-//     }
-// }
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// it didn't let me use sealed/override here, hopefully not a problem..?
+public class LinkedListDemo {
+    private LinkedList<int> nums;
+    public LinkedListDemo(int howManyNums, Random rand) {
+        nums = new LinkedList<int>();
+        for(int i = 0; i < howManyNums; i++) {
+            // is this really an equivalent to nextInt?
+            nums.Add(rand.Next(howManyNums));
+        }
+
+        Console.WriteLine("The first few numbers are: ");
+        for(int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(nums.get(i));
+        }
+    }
+}
